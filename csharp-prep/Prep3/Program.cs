6c40ca5e7dc970718d07b2cbbfadbd4f7cbbde6e
scratchermatch <60678063+scratchermatch@users.noreply.github.com>
2026-09-12 23:04:57 -0600

    made magic number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,30 @@
+// How many of these magic number games have I written in how many languages now? 5? 6?
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+		Random rng = new Random();
+		int magic_number = rng.Next(1, 101); // Inclusive, exclusive
+		int guess;
+
+		while (true) {
+			Console.Write("Guess a number: ");
+			if (! int.TryParse(Console.ReadLine(), out guess)){
+				Console.WriteLine("Not an integer brother, try again");
+				continue;
+			}
+
+			if (guess < magic_number) {
+				Console.WriteLine("higher");
+			} else if (guess > magic_number) {
+				Console.WriteLine("lower");
+			} else {
+				Console.WriteLine("you got it cool");
+				break;
+			}
+		}
+	}
 }
