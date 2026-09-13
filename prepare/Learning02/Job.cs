4279b9e8c15223f6717b6e3b5753f75016236cdc
scratchermatch <60678063+scratchermatch@users.noreply.github.com>
2026-09-13 00:47:26 -0600

    finished abstraction project

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Job {
+	public string _company;
+	public string _job_title;
+	public int _start_year;
+	public int _end_year;
+
+
+    public void Display() {
+        Console.WriteLine($"{_job_title} ({_company}) {_start_year}-{_end_year}");
+    }
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,25 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning02 World!");
+class Program {
+    static void Main(string[] args) {
+		Job job1 = new Job();
+		job1._job_title = "Flavor Tester";
+		job1._company = "Nabisco";
+		job1._start_year = 2010;
+		job1._end_year = 2018;
+
+		Job job2 = new Job();
+		job2._job_title = "CEO";
+		job2._company = "Beijing Corn";
+		job2._start_year = 2019;
+		job2._end_year = 2026;
+
+		Resume resume = new Resume();
+		resume._name = "Buzz Blister";
+
+		resume._jobs.Add(job1);
+		resume._jobs.Add(job2);
+
+		resume.Display();
     }
 }
