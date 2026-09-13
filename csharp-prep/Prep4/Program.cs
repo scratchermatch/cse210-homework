94f118b2e6d54f5390c527c66f5c1cb8da03dc08
scratchermatch <60678063+scratchermatch@users.noreply.github.com>
2026-09-12 23:18:50 -0600

    made list project

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+		int next_int;
+		List<int> numbers = new List<int>();
+
+		while (true) {
+			Console.Write("Type an integer: (type 0 to stop) ");
+			if (!int.TryParse(Console.ReadLine(), out next_int)) {
+				Console.WriteLine("I said to type an integer.");
+				continue;
+			}
+			if (next_int == 0){
+				break;
+			}
+
+			numbers.Add(next_int);
+		}
+
+		int sum = numbers.Sum(); // literally this method exists
+		double average = numbers.Average(); // and this method
+		int max = numbers.Max(); // and this one
+		
+		// A quick trip to the documentation saved about 5 minutes
+		Console.Write($"\nSum: {sum}\nAverage: {average}\nMax: {max}");
     }
 }
